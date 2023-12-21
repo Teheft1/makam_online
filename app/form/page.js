fef98a8ce621054aaf9baceb8a7592943f8bc912
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2023-12-21 17:11:42 +0700

    page form create

---
+++ app/form/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Create from "../components/Form/Create";
+
+const page = () => {
+  return (
+    <div>
+      <Create />
+    </div>
+  );
+};
+
+export default page;
