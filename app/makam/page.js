40c4276253166dfd96cd84cae04a61326215783a
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2023-12-21 17:12:12 +0700

    page list makam

---
+++ app/makam/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Makam from "../components/list/makam";
+
+const page = () => {
+  return (
+    <div>
+      page
+      <Makam />
+    </div>
+  );
+};
+
+export default page;
