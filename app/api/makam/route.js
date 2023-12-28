d860a0066fe17a503e0a918433bac3157c603f3c
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2023-12-28 12:40:03 +0700

    posy api on creating makam

--- app/api/makam/route.js
+++ app/api/makam/route.js
@@ -7,8 +7,45 @@ export async function GET(req, res) {
 }
 
 export async function POST(req, res) {
-    const rest = await req.json();
-    const create = await prisma.makam.create({
-            
-    })
+  const rest = await req.json();
+  console.log(rest);
+  const {
+    ID_pemakaman,
+    ID_Kelas_Makam,
+    ID_PJ,
+    Nomor_Makam,
+    Nama_belasungkawa,
+    Tanggal_Wafat,
+    Status_Makam,
+  } = rest;
+  const makam = await prisma.makam.create({
+    data: {
+      Nomor_Makam: Nomor_Makam,
+      pemakaman: ID_pemakaman,
+      kelas_makam: ID_Kelas_Makam,
+      pj_makam: ID_PJ,
+      Nama_belasungkawa: Nama_belasungkawa || null,
+      Tanggal_Wafat: Tanggal_Wafat ? new Date(Tanggal_Wafat) : null,
+      Status_Makam: Status_Makam || null,
+      pemakaman: {
+        connect: {
+          ID_Pemakaman: ID_pemakaman,
+        },
+      },
+      kelas_makam: {
+        connect: {
+          ID_kelas_makam: ID_Kelas_Makam,
+        },
+      },
+      pj_makam: {
+        connect: {
+          ID_PJ: ID_PJ,
+        },
+      },
+      catatan_makam: null,
+    },
+  });
+  if (!makam) return NextResponse.error(new Error("Gagal membuat makam"));
+
+  return NextResponse.json(makam);
 }
